11d1ab136aed8bff4c79afedd3310394a1bf3d0a
XiangLuc <xiangluc01@gmail.com>
2024-03-26 21:34:44 +0100

    ajout d'un schéma qui va permettre lors d'une insertion de vérifier si les données envoyé respecte le schéma

--- joueur_routes.py
+++ joueur_routes.py
@@ -1,9 +1,29 @@
 from flask import Blueprint, jsonify, request
+from flask_expects_json import expects_json
 from mongo_client import Mongo2Client
 from bson import ObjectId
 
 
 joueurs_bp = Blueprint('joueur_bp', __name__)
+
+joueur_schema = {
+    'type': 'object',
+    'properties': {
+        'nom': {'type': 'string'},
+        'prenom': {'type': 'string'},
+        'categorie': {
+            'type': 'object',
+            'properties': {
+                'age': {'type': 'integer'},
+                'niveau': {'type': 'string'}
+            },
+            'required': ['age', 'niveau']
+        },
+        'sexe': {'type': 'string'},
+        'point': {'type': 'integer'}
+    },
+    'required': ['nom', 'prenom', 'categorie', 'sexe', 'point']
+}
 
 
 @joueurs_bp.route('/', methods=['GET'])
@@ -31,8 +51,9 @@ def get_joueur_by_id(id_joueur):
 
 
 @joueurs_bp.route('/add', methods=['POST'])
+@expects_json(joueur_schema)
 def add_joueur():
-    data = request.get_json()
+    data = request.json
     with Mongo2Client() as mongo_client:
         db_joueur = mongo_client.db['joueur']
         insert_joueur = db_joueur.insert_one(data)
@@ -49,9 +70,9 @@ def delete_joueur_by_id(id_joueur):
         delete_joueur = db_joueur.delete_one({'_id': ObjectId(id_joueur)})
 
         if delete_joueur:
-            return jsonify({"True": "La suppression a bien été réalisée."})
+            return jsonify({"message": "La suppression a bien été réalisée."})
         else:
-            return jsonify({'False': 'Erreur lors de la suppression'}), 404
+            return jsonify({'message': 'Erreur lors de la suppression'}), 404
 
 
 @joueurs_bp.route('/<string:id_joueur>', methods=['PUT'])
@@ -63,6 +84,6 @@ def update_joueur_by_id(id_joueur):
         update_joueur = db_joueur.update_one({'_id': ObjectId(id_joueur)}, {'$set': data})
 
         if update_joueur.modified_count > 0:
-            return jsonify({"True": "La mise à jour a bien été réalisée."})
+            return jsonify({"message": "La mise à jour a bien été réalisée."})
         else:
-            return jsonify({'False': 'Erreur lors de la mise à jour'}), 404
+            return jsonify({'message': 'Erreur lors de la mise à jour'}), 404
